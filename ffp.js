99be4047e2faf4e28f0231654e8bb9559007911b
Dave Hall <dave.hall@skwashd.com>
2014-06-29 12:41:14 +1000

    Fairfax have changed their paywall, so now we just block cookies and js for their sites

--- ffp.js
+++ ffp.js
@@ -1,5 +1,25 @@
-var overlay = document.getElementById('subscription-overlay');
-if (null !== overlay && overlay.parentNode) {
-  overlay.parentNode.removeChild(overlay);
-  document.getElementsByTagName('body')[0].style.overflow = 'auto';
-}
+chrome.runtime.onInstalled.addListener(function () {
+  var domainSnippets = [
+    'theage',
+    'smh',
+    'brisbanetimes',
+    'watoday',
+    'canberratimes'
+  ],
+    prefixes = ['', '*.'],
+    pattern = '',
+    settings = {};
+
+  domainSnippets.forEach(function(snippet) {
+    prefixes.forEach(function(prefix) {
+      pattern = '*://' + prefix + snippet + '.com.au/*';
+      settings = {
+        primaryPattern: pattern,
+        setting: 'block'
+      };
+
+      chrome.contentSettings['javascript'].set(settings);
+      chrome.contentSettings['cookies'].set(settings);
+    });
+  });
+});
