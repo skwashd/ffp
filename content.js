36901749d8a8d6745fa42f06c5457cae8abc8b9e
Dave Hall <dave.hall@skwashd.com>
2014-06-30 18:36:08 +1000

    [#1] Remove panel menus from section headings on homepage

--- content.js
+++ content.js
@@ -1,7 +1,17 @@
-var unwanted = ['box-network', 'box-contact', 'Tvguide'];
-unwanted.forEach(function(id) {
-  var elm = document.getElementById(id);
+function removeElm(elm) {
   if (null !== elm && elm.parentNode) {
     elm.parentNode.removeChild(elm);
   }
+};
+
+var unwantedIds = ['box-network', 'box-contact', 'Tvguide'];
+unwantedIds.forEach(function(id) {
+  removeElm(document.getElementById(id));
 });
+
+// Remove the dropdown panel menus from each section heading on the homepage.
+var unwantedPanels = document.querySelectorAll('div.dropdown-panel');
+for (var i = 0; i < unwantedPanels.length; ++i) {
+  removeElm(unwantedPanels[i]);
+}
+
